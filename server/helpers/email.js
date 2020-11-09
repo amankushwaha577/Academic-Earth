32eaf39a215a63ce7d43b75f8b6dbb21d15eb0dd
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-09 22:40:28 +0530

    Email Messaging

--- server/helpers/email.js
+++ server/helpers/email.js
@@ -5,7 +5,19 @@ exports.registerEmailParams = (email, token) => {
             ToAddresses: [email]
         },
         ReplyToAddresses: [process.env.EMAIL_FROM],
-      
+        Message: {
+            Body: {
+                Html: {
+                    Charset: 'UTF-8',
+                    Data: `
+                        <html>
+                            <h1>Vefiry your email address</h1>
+                            <p>Please use the following link to complete your registration:</p>
+                            <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+                        </html>
+                    `
+                }
+            },
             Subject: {
                 Charset: 'UTF-8',
                 Data: 'Complete your registration'

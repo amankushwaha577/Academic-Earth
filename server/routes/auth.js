f564030a20fdb331fded835429ad7bd86824650a
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-09 22:50:57 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -20,11 +20,6 @@ const {
     resetPassword
 } = require('../controllers/auth');
 
-router.post('/register', userRegisterValidator, runValidation, register);
-router.post('/register/activate', registerActivate);
-router.post('/login', userLoginValidator, runValidation, login);
-router.put('/forgot-password', forgotPasswordValidator, runValidation, forgotPassword);
-router.put('/reset-password', resetPasswordValidator, runValidation, resetPassword);
 
 // router.get('/secret', requireSignin, (req, res) => {
 //     res.json({

0a62fc25935124a705195fdfdbb867d5c94fcf1e
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-09 22:42:51 +0530

    Update email.js

--- server/helpers/email.js
+++ server/helpers/email.js
@@ -28,28 +28,7 @@ exports.registerEmailParams = (email, token) => {
 
 exports.forgotPasswordEmailParams = (email, token) => {
     return {
-        Source: process.env.EMAIL_FROM,
-        Destination: {
-            ToAddresses: [email]
-        },
-        ReplyToAddresses: [process.env.EMAIL_FROM],
-        Message: {
-            Body: {
-                Html: {
-                    Charset: 'UTF-8',
-                    Data: `
-                        <html>
-                            <h1>Reset Password Link</h1>
-                            <p>Please use the following link to reset your password:</p>
-                            <p>${process.env.CLIENT_URL}/auth/password/reset/${token}</p>
-                        </html>
-                    `
-                }
-            },
-            Subject: {
-                Charset: 'UTF-8',
-                Data: 'Password reset link'
-            }
+      
         }
     };
 };

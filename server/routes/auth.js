cdddfbac12b58671b92bb58c65e2fd91013a3b6f
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-09 22:52:43 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,14 +11,6 @@ const {
 const { runValidation } = require('../validators');
 
 // import from controllers
-const {
-    register,
-    registerActivate,
-    login,
-    requireSignin,
-    forgotPassword,
-    resetPassword
-} = require('../controllers/auth');
 
 router.post('/register', userRegisterValidator, runValidation, register);
 router.post('/register/activate', registerActivate);

8b8aa8f8879201a2c02441bf4342c4ee548ef463
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-09 22:32:22 +0530

    Profile update

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -124,14 +124,7 @@ exports.update = (req, res) => {
             });
 
             // handle upload image
-            const params = {
-                Bucket: 'academicearth',
-                Key: `category/${uuidv4()}.${type}`,
-                Body: base64Data,
-                ACL: 'public-read',
-                ContentEncoding: 'base64',
-                ContentType: `image/${type}`
-            };
+           
 
             s3.upload(params, (err, data) => {
                 if (err) {

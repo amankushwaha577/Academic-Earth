30e27695a314687af719a0f6ee374513aa7a7707
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-09 22:54:17 +0530

    User Registration validator

--- server/validators/auth.js
+++ server/validators/auth.js
@@ -1,20 +1,6 @@
 const { check } = require('express-validator');
 
-exports.userRegisterValidator = [
-    check('name')
-        .not()
-        .isEmpty()
-        .withMessage('Name is required'),
-    check('email')
-        .isEmail()
-        .withMessage('Must be a valid email address'),
-    check('password')
-        .isLength({ min: 6 })
-        .withMessage('Password must be at least 6 characters long'),
-    check('categories')
-        .isLength({ min: 6 })
-        .withMessage('Pick at aleast one category')
-];
+
 
 exports.userLoginValidator = [
     check('email')

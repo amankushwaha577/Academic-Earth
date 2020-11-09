89361ed65a556ffc8b42bfb11e854d2fd7e51da0
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-09 22:00:18 +0530

    Controller Authorization

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -228,4 +228,20 @@ exports.resetPassword = (req, res) => {
     }
 };
 
-
+exports.canUpdateDeleteLink = (req, res, next) => {
+    const { id } = req.params;
+    Link.findOne({ _id: id }).exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Could not find link'
+            });
+        }
+        let authorizedUser = data.postedBy._id.toString() === req.user._id.toString();
+        if (!authorizedUser) {
+            return res.status(400).json({
+                error: 'You are not authorized'
+            });
+        }
+        next();
+    });
+};
